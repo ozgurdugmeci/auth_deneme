0220d5646ecf87dd490f448f7c751cd6adf1a8e5
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2025-03-11 14:32:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,14 @@
 import streamlit as st
 'Hey'
+
+if not st.experimental_user.is_logged_in:
+    if st.button("Log in"):
+        st.login()
+else:
+    if st.button("Log out"):
+        st.logout()
+    st.write(f"Hello, {st.experimental_user.name}!")
+
 user =st.experimental_user
 user
 
