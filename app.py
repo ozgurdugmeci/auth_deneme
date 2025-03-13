cf0d691f674df511ceb5b60196a6133d13117e65
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2025-03-13 09:28:15 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 'Hop'
+st.login("google")
 
-
-if not st.experimental_user.is_logged_in:
+'''if not st.experimental_user.is_logged_in:
     if st.button("Log in"):
         st.login("google")
 else:
     if st.button("Log out"):
         st.logout()
-    st.write(f"Hello, {st.experimental_user.name}!")
+    st.write(f"Hello, {st.experimental_user.name}!")'''
 
 #user =st.experimental_user
 #user
