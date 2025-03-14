3e0073b96f0eb02d638d7732f1845489818caa40
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2025-03-14 15:05:43 +0300

    Update app.py

--- app.py
+++ app.py
@@ -9,21 +9,11 @@ st.set_page_config(
 )
 
 # Define app pages
-landing_page = st.Page("./pages/sayfa1.py", title="Landing", icon=":material/home:")
+landing_page = st.Page("./pages/landing_page.py", title="Landing", icon=":material/home:")
 app_page = st.Page("./pages/operator.py", title="App", icon=":material/play_arrow:")
 random_page = st.Page(
-    "./pages/other_page.py", title="Admin", icon=":material/admin_panel_settings:"
+    "./pages/info_page.py", title="Admin", icon=":material/admin_panel_settings:"
 )
-
-# Enables switch_page behaviour
-if not st.experimental_user.is_logged_in:
-    if st.button("Log in"):
-        st.login("google")
-else:
-    if st.button("Log out"):
-        st.logout()
-    st.write(f"Hello, {st.experimental_user.name}!")'''
-
 
 
 
@@ -32,7 +22,7 @@ if not st.experimental_user.is_logged_in:
         [landing_page],
         position="hidden",
     )
-elif st.experimental_user.email == 'ozgur.dugmeci@gmail.com':
+elif st.experimental_user.email == 'ozgurr.dugmeci@gmail.com':
     pg = st.navigation(
         [admin_page],
     )
@@ -44,6 +34,16 @@ else:
 
 # Head to first page of navigation
 pg.run()
+
+# Enables switch_page behaviour
+'''if not st.experimental_user.is_logged_in:
+    if st.button("Log in"):
+        st.login("google")
+else:
+    if st.button("Log out"):
+        st.logout()
+    st.write(f"Hello, {st.experimental_user.name}!")'''
+
 
 
 #user =st.experimental_user
