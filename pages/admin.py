3d3fb7f96833b0250feed8d25bdf05da94f65761
Özgür Düğmeci <ozgur.dugmeci@gmail.com>
2025-03-14 15:29:23 +0300

    Update admin.py

--- pages/admin.py
+++ pages/admin.py
@@ -1,9 +1,2 @@
-import streamlit as st
-if not st.experimental_user.is_logged_in:
-    if st.button("Log in"):
-        st.login("google")
-else:
-    if st.button("Log out"):
-        st.logout()
-    st.write(f"Hello, {st.experimental_user.name}!")'''
+'Hello admin'
 
